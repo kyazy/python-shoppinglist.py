e64c8564fd35d765ed04f593a9714821580dcb4c
kyaz <nico.gross@edu.techstarter.de>
2024-10-02 15:31:30 +0200

    Artikel add

---
+++ shoppinglist.py
@@ -0,0 +1,4 @@
+def add_item():
+    print(f"Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll")
+    item=input("")
+    print(f"Der Artikel {item} wurde der Einkaufsliste hinzugefügt")
