e7c5666da372ca01474f829ebb254caa75802746
kyaz <nico.gross@edu.techstarter.de>
2024-10-02 15:38:17 +0200

    Feature funktion added

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,3 +2,10 @@ def add_item():
     print(f"Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll")
     item=input("")
     print(f"Der Artikel {item} wurde der Einkaufsliste hinzugefügt")
+
+def show_shoppinglist():
+    if shoppinglist:
+        print(f"Deine Einkaufsliste:")
+        for item in shoppinglist:
+            print(f"{item}")
+            else print(f"Deine Einkaufsliste ist leer.")
